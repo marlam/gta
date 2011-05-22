226a824332686a118a245a42c8326a4037279051
Martin Lambers <marlam@marlam.de>
2011-05-22 15:09:05 +0200

    gtatool from-magick: fix typo.

--- gtatool/src/conv-magick/from-magick.cpp
+++ gtatool/src/conv-magick/from-magick.cpp
@@ -41,7 +41,7 @@ extern "C" void gtatool_from_magick_help(void)
 {
     msg::req_txt("from-magick [--force-format=l|la|rgb|rgba] <input-file> [<output-file>]\n"
             "\n"
-            "Convers images readable by ImageMagick to GTAs.");
+            "Converts images readable by ImageMagick to GTAs.");
 }
 
 extern "C" int gtatool_from_magick(int argc, char *argv[])
