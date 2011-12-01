1a4e3e7902f47502cc91e87ad4cc1bc7a85b0b6f
Martin Lambers <marlam@marlam.de>
2011-12-01 14:53:40 +0100

    gtatool: info: implement --statistics.

--- gtatool/src/array/info.cpp
+++ gtatool/src/array/info.cpp
@@ -24,6 +24,7 @@
 #include <sstream>
 #include <cstdio>
 #include <cctype>
+#include <cmath>
 
 #include <gta/gta.hpp>
 
@@ -38,9 +39,12 @@
 extern "C" void gtatool_info_help(void)
 {
     msg::req_txt(
-            "info [<files...>]\n"
+            "info [-s|--statistics] [<files...>]\n"
             "\n"
-            "Print information about GTAs.");
+            "Print information about GTAs.\n"
+            "If --statistics is given, simple statistics about the values in each component "
+            "are computed and printed (in double precision, regardless of input type). Values "
+            "that are not finite numbers are ignored.");
 }
 
 extern "C" int gtatool_info(int argc, char *argv[])
@@ -48,6 +52,8 @@ extern "C" int gtatool_info(int argc, char *argv[])
     std::vector<opt::option *> options;
     opt::info help("help", '\0', opt::optional);
     options.push_back(&help);
+    opt::flag statistics("statistics", 's', opt::optional);
+    options.push_back(&statistics);
     std::vector<std::string> arguments;
     if (!opt::parse(argc, argv, options, -1, -1, arguments))
     {
@@ -65,8 +71,136 @@ extern "C" int gtatool_info(int argc, char *argv[])
         gta::header hdr;
         std::string name;
         array_loop.start(arguments, "");
+        std::vector<double> minima;
+        std::vector<double> maxima;
+        std::vector<double> sum;
+        std::vector<double> squaresum;
         while (array_loop.read(hdr, name))
         {
+            if (statistics.value() && !hdr.data_size() == 0)
+            {
+                minima.clear();
+                maxima.clear();
+                sum.clear();
+                squaresum.clear();
+                minima.resize(checked_cast<size_t>(hdr.components()), +std::numeric_limits<double>::max());
+                maxima.resize(checked_cast<size_t>(hdr.components()), -std::numeric_limits<double>::max());
+                sum.resize(checked_cast<size_t>(hdr.components()), 0.0);
+                squaresum.resize(checked_cast<size_t>(hdr.components()), 0.0);
+                element_loop_t element_loop;
+                array_loop.start_element_loop(element_loop, hdr, hdr);
+                for (uintmax_t e = 0; e < hdr.elements(); e++)
+                {
+                    const void *element = element_loop.read();
+                    for (uintmax_t c = 0; c < hdr.components(); c++)
+                    {
+                        const void *component = hdr.component(element, c);
+                        double val;
+                        switch (hdr.component_type(c))
+                        {
+                        case gta::int8:
+                            {
+                                int8_t v;
+                                memcpy(&v, component, sizeof(int8_t));
+                                val = v;
+                            }
+                            break;
+                        case gta::uint8:
+                            {
+                                uint8_t v;
+                                memcpy(&v, component, sizeof(uint8_t));
+                                val = v;
+                            }
+                            break;
+                        case gta::int16:
+                            {
+                                int16_t v;
+                                memcpy(&v, component, sizeof(int16_t));
+                                val = v;
+                            }
+                            break;
+                        case gta::uint16:
+                            {
+                                uint16_t v;
+                                memcpy(&v, component, sizeof(uint16_t));
+                                val = v;
+                            }
+                            break;
+                        case gta::int32:
+                            {
+                                int32_t v;
+                                memcpy(&v, component, sizeof(int32_t));
+                                val = v;
+                            }
+                            break;
+                        case gta::uint32:
+                            {
+                                uint32_t v;
+                                memcpy(&v, component, sizeof(uint32_t));
+                                val = v;
+                            }
+                            break;
+                        case gta::int64:
+                            {
+                                int64_t v;
+                                memcpy(&v, component, sizeof(int64_t));
+                                val = v;
+                            }
+                            break;
+                        case gta::uint64:
+                            {
+                                uint64_t v;
+                                memcpy(&v, component, sizeof(uint64_t));
+                                val = v;
+                            }
+                            break;
+                        case gta::float32:
+                            {
+                                float v;
+                                memcpy(&v, component, sizeof(float));
+                                val = v;
+                            }
+                            break;
+                        case gta::float64:
+                            {
+                                memcpy(&val, component, sizeof(double));
+                            }
+                            break;
+                        case gta::cfloat32:
+                            {
+                                float v[2];
+                                memcpy(v, component, 2 * sizeof(float));
+                                val = std::sqrt(v[0] * v[0] + v[1] * v[1]);
+                            }
+                            break;
+                        case gta::cfloat64:
+                            {
+                                double v[2];
+                                memcpy(v, component, 2 * sizeof(double));
+                                val = std::sqrt(v[0] * v[0] + v[1] * v[1]);
+                            }
+                            break;
+                        default:
+                            throw exc(std::string("cannot compute minimum/maximum for component type ")
+                                    + type_to_string(hdr.component_type(c), hdr.component_size(c)));
+                            break;
+                        }
+                        if (std::isfinite(val))
+                        {
+                            if (val < minima[c])
+                                minima[c] = val;
+                            else if (val > maxima[c])
+                                maxima[c] = val;
+                            sum[c] += val;
+                            squaresum[c] += val * val;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                array_loop.skip_data(hdr);
+            }
             std::stringstream dimensions;
             for (uintmax_t i = 0; i < hdr.dimensions(); i++)
             {
@@ -95,7 +229,7 @@ extern "C" int gtatool_info(int argc, char *argv[])
                         + str::from(hdr.data_size()) + " bytes ("
                         + str::human_readable_memsize(hdr.data_size()) + ")");
             }
-            msg::req(std::string("    compression: ") +
+            msg::req(4, std::string("compression: ") +
                     (hdr.compression() == gta::none ? "none"
                      : hdr.compression() == gta::zlib ? "zlib default level"
                      : hdr.compression() == gta::bzip2 ? "bzip2"
@@ -111,38 +245,45 @@ extern "C" int gtatool_info(int argc, char *argv[])
                      : hdr.compression() == gta::zlib9 ? "zlib level 9" : "unknown"));
             if (hdr.data_size() == 0)
             {
-                msg::req("    empty array");
+                msg::req(4, "empty array");
             }
             else
             {
-                msg::req(std::string("    ") + dimensions.str() + " elements of type " + components.str());
+                msg::req(4, dimensions.str() + " elements of type " + components.str());
             }
             for (uintmax_t i = 0; i < hdr.global_taglist().tags(); i++)
             {
-                msg::req(std::string("        ")
-                        + from_utf8(hdr.global_taglist().name(i)) + "=" + from_utf8(hdr.global_taglist().value(i)));
+                msg::req(8, from_utf8(hdr.global_taglist().name(i)) + "=" + from_utf8(hdr.global_taglist().value(i)));
             }
             for (uintmax_t i = 0; i < hdr.dimensions(); i++)
             {
-                msg::req(std::string("    dimension ") + str::from(i) + ": " + str::from(hdr.dimension_size(i)));
+                msg::req(4, std::string("dimension ") + str::from(i) + ": " + str::from(hdr.dimension_size(i)));
                 for (uintmax_t j = 0; j < hdr.dimension_taglist(i).tags(); j++)
                 {
-                    msg::req(std::string("        ")
-                            + from_utf8(hdr.dimension_taglist(i).name(j)) + "=" + from_utf8(hdr.dimension_taglist(i).value(j)));
+                    msg::req(8, from_utf8(hdr.dimension_taglist(i).name(j)) + "=" + from_utf8(hdr.dimension_taglist(i).value(j)));
                 }
             }
             for (uintmax_t i = 0; i < hdr.components(); i++)
             {
-                msg::req(std::string("    element component ") + str::from(i) + ": "
+                msg::req(4, std::string("element component ") + str::from(i) + ": "
                         + type_to_string(hdr.component_type(i), hdr.component_size(i)) + ", "
                         + str::human_readable_memsize(hdr.component_size(i)));
+                if (statistics.value() && !hdr.data_size() == 0)
+                {
+                    msg::req(8, std::string("minimum value = ") + str::from(minima[i]));
+                    msg::req(8, std::string("maximum value = ") + str::from(maxima[i]));
+                    double mean = sum[i] / (hdr.elements() - 1);
+                    double variance = (squaresum[i] - sum[i] / hdr.elements() * sum[i]) / (hdr.elements() - 1);
+                    double deviation = std::sqrt(variance);
+                    msg::req(8, std::string("sample mean = ") + str::from(mean));
+                    msg::req(8, std::string("sample variance = ") + str::from(variance));
+                    msg::req(8, std::string("sample deviation = ") + str::from(deviation));
+                }
                 for (uintmax_t j = 0; j < hdr.component_taglist(i).tags(); j++)
                 {
-                    msg::req(std::string("        ")
-                            + from_utf8(hdr.component_taglist(i).name(j)) + "=" + from_utf8(hdr.component_taglist(i).value(j)));
+                    msg::req(8, from_utf8(hdr.component_taglist(i).name(j)) + "=" + from_utf8(hdr.component_taglist(i).value(j)));
                 }
             }
-            array_loop.skip_data(hdr);
         }
         array_loop.finish();
     }
