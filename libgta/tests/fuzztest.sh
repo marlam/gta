75528da918eb4b8073ac9077cf756e6a017d9da6
Martin Lambers <marlam@marlam.de>
2011-12-09 06:42:03 +0100

    libgta: fuzz test: clean up when interrupted

--- libgta/tests/fuzztest.sh
+++ libgta/tests/fuzztest.sh
@@ -6,13 +6,16 @@ if test $# -eq 0; then
     exit 77
 fi
 
+dir="`mktemp -d fuzztest-XXXXXX`"
+trap "rm -r $dir" INT
+
 startsec="`date +%s`"
 #seed="$startsec"
 seed="`od -vAn -N4 -tu4 < /dev/urandom`"
 i=0
 while true; do
-    validgta="`mktemp fuzztest-valid-XXXXXX.gta`"
-    corruptgta="`mktemp fuzztest-corrupt-XXXXXX.gta`"
+    validgta="$dir/fuzztest-valid.gta"
+    corruptgta="$dir/fuzztest-corrupt.gta"
     newseed="`./fuzztest-create "$seed" "$validgta" "$corruptgta"`"
     if test $? -ne 0; then
         echo "Failed to create fuzz test files."
