35d1c3c1f5ba1a68f6036b870bd40251f1667e0f
Martin Lambers <marlam@marlam.de>
2014-01-06 12:54:04 +0100

    gta from-raw: add several options for finer control.
    This adds --stream-skip, --array-pre-skip, --array-post-skip.

--- gtatool/src/conv-raw/from-raw.cpp
+++ gtatool/src/conv-raw/from-raw.cpp
@@ -2,7 +2,7 @@
  * This file is part of gtatool, a tool to manipulate Generic Tagged Arrays
  * (GTAs).
  *
- * Copyright (C) 2010, 2011, 2012, 2013
+ * Copyright (C) 2010, 2011, 2012, 2013, 2014
  * Martin Lambers <marlam@marlam.de>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -38,12 +38,18 @@
 extern "C" void gtatool_from_raw_help(void)
 {
     msg::req_txt("from-raw -d|--dimensions=<d0,d1,...> -c|--components=<c0,c1,...>\n"
-            "    [-n|--n=<n>] [-e|--endianness=little|big] <input-file> [<output-file>]\n"
+            "    [-n|--n=<n>] [-e|--endianness=little|big]\n"
+            "    [--stream-skip=<bytes>] [--array-pre-skip=<bytes>] [--array-post-skip=<bytes>]\n"
+            "    <input-file> [<output-file>]\n"
             "\n"
             "Converts raw binary files to GTAs.\n"
             "You need to specify the dimensions (-d) and components (-c) of the data.\n"
             "You can set the number n of GTAs to create; by default, GTAs are created as long "
             "as the input provides more data.\n"
+            "If the data stream contains more than just the raw array data, you can specify "
+            "byte ranges that should be ignored. The --stream-skip option skips the given number of "
+            "bytes at the beginning of the stream. The --array-pre-skip/--array-post-skip options skip "
+            "the given number of bytes before and after each array.\n          "
             "The default endianness is little.\n"
             "Available component types: int8, uint8, int16, uint16, int32, uint32, "
             "int64, uint64, int128, uint128, float32, float64, float128, cfloat32, "
@@ -67,6 +73,12 @@ extern "C" int gtatool_from_raw(int argc, char *argv[])
     endiannesses.push_back("big");
     opt::val<std::string> endian("endianness", 'e', opt::optional, endiannesses, "little");
     options.push_back(&endian);
+    opt::val<off_t> stream_skip("stream-skip", '\0', opt::optional, 0);
+    options.push_back(&stream_skip);
+    opt::val<off_t> array_pre_skip("array-pre-skip", '\0', opt::optional, 0);
+    options.push_back(&array_pre_skip);
+    opt::val<off_t> array_post_skip("array-post-skip", '\0', opt::optional, 0);
+    options.push_back(&array_post_skip);
     std::vector<std::string> arguments;
     if (!opt::parse(argc, argv, options, 1, 2, arguments))
     {
@@ -101,10 +113,18 @@ extern "C" int gtatool_from_raw(int argc, char *argv[])
         array_loop.start(std::vector<std::string>(1, arguments[0]), arguments.size() == 2 ? arguments[1] : "");
         std::string nameo;
 
+        if (stream_skip.value() > 0)
+        {
+            fio::seek(array_loop.file_in(), stream_skip.value(), SEEK_CUR, array_loop.filename_in());
+        }
         do
         {
             array_loop.write(hdr, nameo);
 
+            if (array_pre_skip.value() > 0)
+            {
+                fio::seek(array_loop.file_in(), array_pre_skip.value(), SEEK_CUR, array_loop.filename_in());
+            }
             if (host_endianness)
             {
                 array_loop.copy_data(hdr, hdr);
@@ -120,6 +140,10 @@ extern "C" int gtatool_from_raw(int argc, char *argv[])
                     swap_element_endianness(hdr, element.ptr());
                     element_loop.write(element.ptr());
                 }
+            }
+            if (array_post_skip.value() > 0)
+            {
+                fio::seek(array_loop.file_in(), array_post_skip.value(), SEEK_CUR, array_loop.filename_in());
             }
         }
         while ((n.value() == 0 && fio::has_more(array_loop.file_in()))
